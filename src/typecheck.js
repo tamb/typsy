214426236dcfeab3ef84e257ba0fbd99dd2944cf
tamb <tamb89@protonmail.com>
2019-12-07 10:39:35 -0500

    Adds more tests for typeCheck

--- src/typecheck.js
+++ src/typecheck.js
@@ -2,18 +2,17 @@ export default function typeCheck(item) {
   return {
     item,
     isInteger: () => typeof item === "number" && item % 1 === 0,
-    isNegativeInteger: () =>
-      typeof item === "number" && item % 1 === 0 && item < 0,
     isString: () => typeof item === "string",
     isEmptyString: () => typeof item === "string" && item.length === 0,
+    isAllWhiteSpace: () => typeof item === "string" && !/\S/.test(item),
     isBoolean: () => typeof item === "boolean",
     isFloat: () => typeof item === "number" && item % 1 !== 0,
-    isNegativeFloat: () =>
-      typeof item === "number" && item % 1 !== 0 && item < 0,
-    isObject: () => typeof item === "object" && item !== null,
+    isObject: () =>
+      typeof item === "object" && item !== null && !Array.isArray(item),
     isNull: () => typeof item === "object" && item === null,
     isUndefined: () => typeof item === "undefined" && item === undefined,
     isArray: () => Array.isArray(item),
-    isEmptyArray: () => Array.isArray(item) && item.length === 0
+    isEmptyArray: () => Array.isArray(item) && item.length === 0,
+    isFunction: () => typeof item === "function"
   };
 }
--- src/typecheck.test.js
+++ src/typecheck.test.js
@@ -1,20 +1,78 @@
 import typeCheck from "./typecheck";
 
-describe("typeCheck function works", () => {
-  test("Integer test to return true for Negative and Positive Integer", () => {
+describe("typeCheck", () => {
+  test("Integer test to return true for Negative and Positive Integer.  String is not an Int", () => {
     expect(typeCheck(12).isInteger()).toBe(true);
     expect(typeCheck(-12).isInteger()).toBe(true);
+    expect(typeCheck(null).isInteger()).toBe(false);
   });
-  // let x;
-  // console.log("Str", typeCheck("hello").isString());
-  // console.log("Bool", typeCheck(true).isBoolean());
-  // console.log("Flt", typeCheck(1.1).isFloat());
-  // console.log("Null", typeCheck(null).isNull());
-  // console.log("Obj", typeCheck({}).isObject());
-  // console.log("Undf", typeCheck(x).isUndefined());
-  // console.log("Arr", typeCheck([1, 2]).isArray());
-  // console.log("Empty Arr", typeCheck([]).isEmptyArray());
-  // console.log("Empty Str", typeCheck("").isEmptyString());
-  // console.log("Neg Int", typeCheck(-12).isNegativeInteger());
-  // console.log("Neg Flt", typeCheck(-1.1).isNegativeFloat());
+  test("String to be string and int to be false", () => {
+    expect(typeCheck("Is string").isString()).toBe(true);
+    expect(typeCheck(-12).isString()).toBe(false);
+  });
+
+  test("Empty String to be true, any characters to be false", () => {
+    expect(typeCheck("").isEmptyString()).toBe(true);
+    expect(typeCheck("    ").isEmptyString()).toBe(false);
+    expect(typeCheck("hey").isEmptyString()).toBe(false);
+  });
+
+  test("String with all whitespace is true", () => {
+    expect(typeCheck("").isAllWhiteSpace()).toBe(true);
+    expect(typeCheck("    ").isAllWhiteSpace()).toBe(true);
+    expect(typeCheck("    hey    ").isAllWhiteSpace()).toBe(false);
+  });
+
+  test("Booleans are true, booleans as string are false", () => {
+    expect(typeCheck(false).isBoolean()).toBe(true);
+    expect(typeCheck(true).isBoolean()).toBe(true);
+    expect(typeCheck(0).isBoolean()).toBe(false);
+    expect(typeCheck("false").isBoolean()).toBe(false);
+    expect(typeCheck("true").isBoolean()).toBe(false);
+  });
+
+  test("Only a strict object, not an array, is an object", () => {
+    expect(typeCheck({}).isObject()).toBe(true);
+    expect(typeCheck(new Array()).isObject()).toBe(false);
+    expect(typeCheck(null).isObject()).toBe(false);
+    expect(typeCheck("tom").isObject()).toBe(false);
+  });
+
+  test("Negative, positive and non-string floats are floats", () => {
+    expect(typeCheck(2.34).isFloat()).toBe(true);
+    expect(typeCheck(-2.34).isFloat()).toBe(true);
+    expect(typeCheck(234).isFloat()).toBe(false);
+    expect(typeCheck("23.23").isFloat()).toBe(false);
+  });
+
+  test("null is null", () => {
+    let x;
+    expect(typeCheck(null).isNull()).toBe(true);
+    expect(typeCheck("null").isNull()).toBe(false);
+    expect(typeCheck("").isNull()).toBe(false);
+    expect(typeCheck(x).isNull()).toBe(false);
+  });
+
+  test("undefined is undefined", () => {
+    let x;
+    expect(typeCheck(x).isUndefined()).toBe(true);
+    expect(typeCheck("undefined").isUndefined()).toBe(false);
+    expect(typeCheck("").isUndefined()).toBe(false);
+    x = 12;
+    expect(typeCheck(x).isUndefined()).toBe(false);
+  });
+
+  test("array is array", () => {
+    let x;
+    expect(typeCheck(x).isArray()).toBe(false);
+    expect(typeCheck("Array").isArray()).toBe(false);
+    expect(typeCheck("[]").isArray()).toBe(false);
+    x = [];
+    expect(typeCheck(x).isArray()).toBe(true);
+    expect(typeCheck(new Array()).isArray()).toBe(true);
+    x = [1, 2, 3];
+    expect(typeCheck(x).isArray()).toBe(true);
+  });
+
+  // TODO - empty array, function
 });
