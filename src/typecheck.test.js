62f1e1368ce6ebdb1a61981bc97a675beadc8977
tamb <tamb89@protonmail.com>
2019-12-08 14:16:15 -0500

    adds chaining. WIP. unless does not work

--- src/typecheck.test.js
+++ src/typecheck.test.js
@@ -2,77 +2,288 @@ import typeCheck from "./typecheck";
 
 describe("typeCheck", () => {
   test("Integer test to return true for Negative and Positive Integer.  String is not an Int", () => {
-    expect(typeCheck(12).isInteger()).toBe(true);
-    expect(typeCheck(-12).isInteger()).toBe(true);
-    expect(typeCheck(null).isInteger()).toBe(false);
+    expect(
+      typeCheck(12)
+        .isInteger()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(-12)
+        .isInteger()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(null)
+        .isInteger()
+        .yields()
+    ).toBe(false);
   });
   test("String to be string and int to be false", () => {
-    expect(typeCheck("Is string").isString()).toBe(true);
-    expect(typeCheck(-12).isString()).toBe(false);
+    expect(
+      typeCheck("Is string")
+        .isString()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(-12)
+        .isString()
+        .yields()
+    ).toBe(false);
   });
 
   test("Empty String to be true, any characters to be false", () => {
-    expect(typeCheck("").isEmptyString()).toBe(true);
-    expect(typeCheck("    ").isEmptyString()).toBe(false);
-    expect(typeCheck("hey").isEmptyString()).toBe(false);
+    expect(
+      typeCheck("")
+        .isEmptyString()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck("    ")
+        .isEmptyString()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("hey")
+        .isEmptyString()
+        .yields()
+    ).toBe(false);
   });
 
   test("String with all whitespace is true", () => {
-    expect(typeCheck("").isAllWhiteSpace()).toBe(true);
-    expect(typeCheck("    ").isAllWhiteSpace()).toBe(true);
-    expect(typeCheck("    hey    ").isAllWhiteSpace()).toBe(false);
+    expect(
+      typeCheck("")
+        .isAllWhiteSpace()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck("    ")
+        .isAllWhiteSpace()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck("    hey    ")
+        .isAllWhiteSpace()
+        .yields()
+    ).toBe(false);
   });
 
   test("Booleans are true, booleans as string are false", () => {
-    expect(typeCheck(false).isBoolean()).toBe(true);
-    expect(typeCheck(true).isBoolean()).toBe(true);
-    expect(typeCheck(0).isBoolean()).toBe(false);
-    expect(typeCheck("false").isBoolean()).toBe(false);
-    expect(typeCheck("true").isBoolean()).toBe(false);
+    expect(
+      typeCheck(false)
+        .isBoolean()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(true)
+        .isBoolean()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(0)
+        .isBoolean()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("false")
+        .isBoolean()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("true")
+        .isBoolean()
+        .yields()
+    ).toBe(false);
   });
 
   test("Only a strict object, not an array, is an object", () => {
-    expect(typeCheck({}).isObject()).toBe(true);
-    expect(typeCheck(new Array()).isObject()).toBe(false);
-    expect(typeCheck(null).isObject()).toBe(false);
-    expect(typeCheck("tom").isObject()).toBe(false);
+    expect(
+      typeCheck({})
+        .isObject()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(new Array())
+        .isObject()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck(null)
+        .isObject()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("tom")
+        .isObject()
+        .yields()
+    ).toBe(false);
   });
 
   test("Negative, positive and non-string floats are floats", () => {
-    expect(typeCheck(2.34).isFloat()).toBe(true);
-    expect(typeCheck(-2.34).isFloat()).toBe(true);
-    expect(typeCheck(234).isFloat()).toBe(false);
-    expect(typeCheck("23.23").isFloat()).toBe(false);
+    expect(
+      typeCheck(2.34)
+        .isFloat()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(-2.34)
+        .isFloat()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(234)
+        .isFloat()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("23.23")
+        .isFloat()
+        .yields()
+    ).toBe(false);
   });
 
   test("null is null", () => {
     let x;
-    expect(typeCheck(null).isNull()).toBe(true);
-    expect(typeCheck("null").isNull()).toBe(false);
-    expect(typeCheck("").isNull()).toBe(false);
-    expect(typeCheck(x).isNull()).toBe(false);
+    expect(
+      typeCheck(null)
+        .isNull()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck("null")
+        .isNull()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("")
+        .isNull()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck(x)
+        .isNull()
+        .yields()
+    ).toBe(false);
   });
 
   test("undefined is undefined", () => {
     let x;
-    expect(typeCheck(x).isUndefined()).toBe(true);
-    expect(typeCheck("undefined").isUndefined()).toBe(false);
-    expect(typeCheck("").isUndefined()).toBe(false);
+    expect(
+      typeCheck(x)
+        .isUndefined()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck("undefined")
+        .isUndefined()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("")
+        .isUndefined()
+        .yields()
+    ).toBe(false);
     x = 12;
-    expect(typeCheck(x).isUndefined()).toBe(false);
+    expect(
+      typeCheck(x)
+        .isUndefined()
+        .yields()
+    ).toBe(false);
   });
 
   test("array is array", () => {
     let x;
-    expect(typeCheck(x).isArray()).toBe(false);
-    expect(typeCheck("Array").isArray()).toBe(false);
-    expect(typeCheck("[]").isArray()).toBe(false);
+    expect(
+      typeCheck(x)
+        .isArray()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("Array")
+        .isArray()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("[]")
+        .isArray()
+        .yields()
+    ).toBe(false);
     x = [];
-    expect(typeCheck(x).isArray()).toBe(true);
-    expect(typeCheck(new Array()).isArray()).toBe(true);
+    expect(
+      typeCheck(x)
+        .isArray()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(new Array())
+        .isArray()
+        .yields()
+    ).toBe(true);
     x = [1, 2, 3];
-    expect(typeCheck(x).isArray()).toBe(true);
+    expect(
+      typeCheck(x)
+        .isArray()
+        .yields()
+    ).toBe(true);
   });
 
-  // TODO - empty array, function
+  test("empty array is true only when empty", () => {
+    let x;
+    expect(
+      typeCheck(x)
+        .isEmptyArray()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck("Array")
+        .isEmptyArray()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck([])
+        .isEmptyArray()
+        .yields()
+    ).toBe(true);
+    x = [];
+    expect(
+      typeCheck(x)
+        .isEmptyArray()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(new Array())
+        .isEmptyArray()
+        .yields()
+    ).toBe(true);
+    x = [1, 2, 3];
+    expect(
+      typeCheck(x)
+        .isEmptyArray()
+        .yields()
+    ).toBe(false);
+  });
+
+  test("function is a function", () => {
+    let x;
+    expect(
+      typeCheck(x)
+        .isFunction()
+        .yields()
+    ).toBe(false);
+    expect(
+      typeCheck(() => {})
+        .isFunction()
+        .yields()
+    ).toBe(true);
+    expect(
+      typeCheck(function Tom() {})
+        .isFunction()
+        .yields()
+    ).toBe(true);
+    x = () => {};
+    expect(
+      typeCheck(x)
+        .isFunction()
+        .yields()
+    ).toBe(true);
+  });
 });
